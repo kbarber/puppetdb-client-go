05b2bdecb9f455e51c50f14bbce504fea58ff4c9
Ken Barber <ken@bob.sh>
2014-01-19 14:02:09 +0000

    Add DeactivateNode function and general cleanup
    Signed-off-by: Ken Barber <ken@bob.sh>

--- commands.go
+++ commands.go
@@ -22,38 +22,73 @@ Where payload contains a variable structure depending on the command itself.
 
 See here for more details on the protocol: http://docs.puppetlabs.com/puppetdb/latest/api/commands.html
 */
-type PayloadObject struct {
+type CommandObject struct {
 	Command string `json:"command"`
 	Version	int `json:"version"`
 	Payload	interface{} `json:"payload"`
 }
 
+/*
+Response to a commands submission request.
+
+This struct contains the fields that are returned when a command was
+successfully submitted. This does not indicate the command was processed,
+just an acknowledgement it was received and will be processed in the future.
+
+More details here: http://docs.puppetlabs.com/puppetdb/latest/api/commands.html#command-submission
+*/
 type CommandResponse struct {
 	Uuid string `json:"uuid"`
 }
 
+/*
+Fact command submission struct for submitting the 'replace facts' command
+to PuppetDB.
+
+More details here: http://docs.puppetlabs.com/puppetdb/latest/api/wire_format/facts_format.html
+*/
 type FactsWireFormat struct {
 	Name string `json:"name"`
 	Values map[string]string `json:"values"`
 }
 
+/*
+Representation of a PuppetDB server instance.
+
+Use NewServer to create a new instance.
+*/
 type Server struct {
 	BaseUrl string
 }
 
+/*
+Create a new instance of a Server for usage later.
+
+This is usually the main entry point of this SDK, where you would create
+this initial object and use it to perform activities on the instance in
+question.
+*/
 func NewServer(baseUrl string) Server {
 	return Server{baseUrl}
 }
 
+/*
+Generic command submission support, for submitting commands to a PuppetDB instance.
+
+This is ordinarily not used, instead its recommended to use the various direct
+functions instead.
+
+More detail here: http://docs.puppetlabs.com/puppetdb/latest/api/commands.html
+*/
 func (server *Server) SubmitCommand(command string, version int, payload string) CommandResponse {
 	baseUrl := server.BaseUrl
 	commandsUrl := strings.Join([]string{baseUrl, "v3/commands"}, "")
 
-	payloadObject := PayloadObject{command, version, payload}
-	postPayload, _ := json.Marshal(payloadObject)
+	commandObject := CommandObject{command, version, payload}
+	commandJson, _ := json.Marshal(commandObject)
 
 	data := url.Values{}
-	data.Set("payload", string(postPayload[:]))
+	data.Set("payload", string(commandJson[:]))
 
 	req, _ := http.NewRequest("POST", commandsUrl, bytes.NewBufferString(data.Encode()))
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
@@ -69,12 +104,33 @@ func (server *Server) SubmitCommand(command string, version int, payload string)
 	return commandResponse
 }
 
-func (server *Server) ReplaceFacts(certname string, facts map[string]string) string {
+/*
+Submit a new 'replace facts' command to PuppetDB.
+
+This function will submit a 'replace facts' command. It accepts a certificate
+name and a map of facts (key/value pairs).
+
+More details here: http://docs.puppetlabs.com/puppetdb/latest/api/commands.html#replace-facts-version-1
+*/
+func (server *Server) ReplaceFacts(certname string, facts map[string]string) CommandResponse {
         factsPayload := FactsWireFormat{certname, facts}
 	factsJson, _ := json.Marshal(factsPayload)
 
 	commandResponse := server.SubmitCommand("replace facts", 1, string(factsJson[:]))
-	uuid := commandResponse.Uuid
+	return commandResponse
+}
 
-	return uuid
+/*
+Submit a new 'deactivate node' command to PuppetDB.
+
+This function will submit a 'deactivate node' command. It accepts a certificate
+name as an argument to indicate which node to deactivate.
+
+More details here: http://docs.puppetlabs.com/puppetdb/latest/api/commands.html#deactivate-node-version-1
+*/
+func (server *Server) DeactivateNode(certname string) CommandResponse {
+	certnameJson, _ := json.Marshal(certname)
+
+	commandResponse := server.SubmitCommand("deactivate node", 1, string(certnameJson[:]))
+	return commandResponse
 }
--- puppetdb.go
+++ puppetdb.go
@@ -1,4 +1,39 @@
 /*
 This package provides a PuppetDB Client in Golang.
+
+An example for submitting a 'replace facts' command:
+
+	package main
+
+	import (
+		"fmt"
+		"github.com/kbarber/puppetdb-client-go"
+	)
+
+	func main() {
+		server := puppetdb.NewServer("http://localhost:8080/")
+
+		facts := map[string]string{
+			"foo": "bar",
+		}
+
+		response := server.ReplaceFacts("foobar", facts)
+		fmt.Printf("UUID: %v\n", response.Uuid)
+	}
+
+An example for submitting a 'deactivate node' command:
+
+	package main
+
+	import (
+		"fmt"
+		"github.com/kbarber/puppetdb-client-go"
+	)
+
+	func main() {
+		server := puppetdb.NewServer("http://localhost:8080/")
+		response := server.DeactivateNode("foobar")
+		fmt.Printf("UUID: %v\n", response.Uuid)
+	}
 */
 package puppetdb
